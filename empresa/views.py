bd4781c51c4280a0c4327c8308ed4b058e3cd0dc
GiuliannT <giuliconti1@gmail.com>
2021-08-04 16:30:47 -0300

    ejemplo funcion render

--- empresa/views.py
+++ empresa/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("Pagina de inicio")
+    contexto = {
+        "nombre":"Axel",
+        "apellido":"Conti",
+        "numero":24
+    }
+    return render(request, "bienvenido/inicio.html", contexto)
+    #return HttpResponse("<h1>Pagina de inicio<h1>")
 
 def saludar(request):
     variable = 5+7
     return HttpResponse(f"Hola mundo {variable}")
+
+def saludar2(request):
+    variable = 4214124+414
+    return HttpResponse(f"Hola mundosdada {variable}")
--- prueba/urls.py
+++ prueba/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from empresa import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.inicio),
-    path("bienvenido/", views.saludar)
+    path("bienvenido/", include("empresa.urls"))
 ]
