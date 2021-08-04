9e9789fec163fb3533eb6572e54b74c684b87c89
GiuliannT <giuliconti1@gmail.com>
2021-08-04 17:16:00 -0300

    include y extends

--- empresa/urls.py
+++ empresa/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.inicio),
     path("bienvenido/", views.saludar),
-    path("otra_url/", views.saludar2)
+    path("otra_url/", views.saludar2),
+    path("numero/<int:numero_x>/", views.ver_numero),
+    path("saludar/<str:nombre>/", views.saludar),
+
+    path("sumar/<int:a>/<int:b>", views.sumar)
 ]
--- empresa/views.py
+++ empresa/views.py
@@ -10,10 +10,27 @@ def inicio(request):
     return render(request, "bienvenido/inicio.html", contexto)
     #return HttpResponse("<h1>Pagina de inicio<h1>")
 
-def saludar(request):
-    variable = 5+7
-    return HttpResponse(f"Hola mundo {variable}")
+def saludar(request, nombre):
+    contexto = {
+        "nombre":nombre
+    }
+    return render(request, "bienvenido/saludar.html", contexto)
 
 def saludar2(request):
     variable = 4214124+414
     return HttpResponse(f"Hola mundosdada {variable}")
+
+def ver_numero(request, numero_x):
+    contexto = {
+        "numero":numero_x
+    }
+    return render(request, "bienvenido/inicio.html", contexto)
+
+def sumar(request, a, b):
+    resultado = a + b
+    contexto = {
+        "a":a,
+        "b":b,
+        "resultado":resultado
+    }
+    return render(request, "bienvenido/suma.html", contexto)
